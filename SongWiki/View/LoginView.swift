923a8b3397718dc12c402fa5fe67cea4db63de99
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-08-22 16:02:42 -0300

    refactor: create markings and rearrange components

--- SongWiki/View/LoginView.swift
+++ SongWiki/View/LoginView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoginView: UIView {
     
+    //MARK: - Components
+    
     lazy var gradient: UIView =  {
         let view = GradientView(colors: [UIColor.systemPurple.cgColor, UIColor(red: 153/255, green: 0/255, blue: 51/255, alpha: 1).cgColor])
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -41,19 +43,6 @@ class LoginView: UIView {
         return button
     }()
     
-    lazy var stackView: UIStackView = {
-        let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .vertical
-        stack.spacing = 20
-        stack.addArrangedSubview(loginTextField)
-        stack.addArrangedSubview(passwordTextField)
-        stack.addArrangedSubview(logInButton)
-        
-        stack.distribution = .fillEqually
-        stack.isUserInteractionEnabled = true
-        return stack
-    }()
     
     lazy var forgottenPasswordButton: UIButton = {
         let button = AuthButton()
@@ -81,6 +70,34 @@ class LoginView: UIView {
         button.setTitleColor( .white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         return button
+    }()
+    
+    lazy var noAccountButton: UIButton = {
+        let button = AuthButton()
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.87), .font: UIFont.systemFont(ofSize: 15)]
+        let attributedTitle = NSMutableAttributedString(string: "Não tem uma conta? ", attributes: atts)
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.87), .font: UIFont.boldSystemFont(ofSize: 15)]
+        attributedTitle.append(NSAttributedString(string: "Registre-se", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.backgroundColor = UIColor(white: 1, alpha: 0)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - StackViews
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.addArrangedSubview(loginTextField)
+        stack.addArrangedSubview(passwordTextField)
+        stack.addArrangedSubview(logInButton)
+        
+        stack.distribution = .fillEqually
+        stack.isUserInteractionEnabled = true
+        return stack
     }()
     
     lazy var secondStackView: UIStackView = {
@@ -120,7 +137,7 @@ class LoginView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    //MARK: -Constraints
     private func configConstraints() {
         NSLayoutConstraint.activate([
             self.gradient.topAnchor.constraint(equalTo: self.topAnchor),
