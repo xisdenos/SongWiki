d74895d95a8e647a757706548e91440fca700e4a
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-08-24 17:29:01 -0300

    feature: protocols for the viewmodels and validations for registerViewModel

--- SongWiki/ViewModel/LoginCycle/RegistrationViewModel.swift
+++ SongWiki/ViewModel/LoginCycle/RegistrationViewModel.swift
@@ -1,27 +1,40 @@
 //
-//  LoginViewModel.swift
+//  RegistrationViewModel.swift
 //  SongWiki
 //
-//  Created by Lucas Pinto on 23/08/22.
+//  Created by Lucas Pinto on 24/08/22.
 //
 
 import Foundation
 import UIKit
 
-struct LoginViewModel {
+struct RegistrationViewModel: FieldValidation {
+    
     var email: String?
+    var user: String?
     var password: String?
+    var passwordConfirmation: String?
+    
     var imputNotEmpty: Bool {
-        return email?.isEmpty == false &&
-        password?.isEmpty == false
+        return
+        email?.isEmpty == false &&
+        user?.isEmpty == false &&
+        password?.isEmpty == false &&
+        passwordConfirmation?.isEmpty == false &&
+        password == passwordConfirmation
     }
+    
     var shouldEnableButton: Bool {
         return imputNotEmpty
     }
+    
     var buttonTitleColor: UIColor {
         return imputNotEmpty ? .white : UIColor(white: 1, alpha: 0.40)
     }
+    
     var buttonBackgroundColor: UIColor {
         return imputNotEmpty ? .systemPurple : .systemPurple.withAlphaComponent(0.5)
     }
+    
+    
 }
