2f2ae9f6e10e969dde371f96d06fa0e54e2e20b0
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-08-27 20:31:17 -0300

    feature: TabBar coordinator

---
+++ SongWiki/Coordinator/TabBar/TabBarCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarCoordinator.swift
+//  SongWiki
+//
+//  Created by Lucas Pinto on 27/08/22.
+//
+
+import UIKit
+
+class TabBarCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+}
