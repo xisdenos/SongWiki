697586d380fdd4eb7321dc033cc225676f84b9e5
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-08-29 16:07:51 -0300

    refactor: api calling closure moved to model

--- SongWiki/Controller/Product/HomeViewController.swift
+++ SongWiki/Controller/Product/HomeViewController.swift
@@ -11,17 +11,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .green
         
-        GenericAdapter.init(connection: NetworkCaller()).adapter(endpoint: "https://theaudiodb.p.rapidapi.com/mostloved.php?format=track") { (album: Result<[TopAlbums], Error>) in
-            switch album {
-            
-            case .success(let albums):
-                print("ok")
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
 }
