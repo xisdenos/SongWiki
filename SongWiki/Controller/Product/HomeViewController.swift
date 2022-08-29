744ed2a4f77a18acb88dbaac3ff9ec2558361692
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-08-29 17:23:26 -0300

    feature: HomeView first layout with collectionView

--- SongWiki/Controller/Product/HomeViewController.swift
+++ SongWiki/Controller/Product/HomeViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    var homeView: HomeView = HomeView()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = homeView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        homeView.collectionViewDelegate(delegate: self, dataSource: self)
     }
 }
+
+//MARK: - Delegate extension
+//extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 6
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        return UICollectionViewCell()
+//    }
+//}
