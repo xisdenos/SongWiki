0da8eb82b6cd308d5dfd66eeae9da73aad51003d
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-10-03 21:23:55 -0300

    tests: add email field validation test

---
+++ SongWikiTests/LoginCycleTests/RegisterModelValidator.swift
@@ -0,0 +1,43 @@
+//
+//  RegisterModelValidator.swift
+//  SongWikiTests
+//
+//  Created by Lucas Pinto on 03/10/22.
+//
+
+import XCTest
+@testable import SongWiki
+
+final class RegisterModelValidator: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testRegisterModelValidator_WhenEmailValidation_ShouldReturnTrue() {
+        // Arrange
+        var sut = RegistrationViewModel()
+        
+        //Act
+        sut.email = "lucas@gmail.com"
+        
+        //Assert
+        XCTAssertTrue(sut.validateEmail(), "The validateEmail() should have returned True when the Email TextField is not empty and contains @ and . ")
+    }
+    
+    func testRegisterModelValidator_WhenUserValidation_ShouldReturnTrue() {
+        // Arrange
+        var sut = RegistrationViewModel()
+        
+        //Act
+        sut.user = "jaime"
+        
+        //Assert
+    }
+}
+
+
